bd70a1e16280050db550b28091b6398f5f8c12a5
Гапеевцев Артём <gapeevtsev.a@promail366.com>
2025-05-25 20:54:12 +0800

    end study

---
+++ handlers/user_handlers.py
@@ -0,0 +1,20 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+from lexicon.lexicon import LEXICON_RU
+
+
+# Инициализируем роутер уровня модуля
+router = Router()
+
+
+# Этот хэндлер срабатывает на команду /start
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU["/start"])
+
+
+# Этот хэндлер срабатывает на команду /help
+@router.message(Command(commands="help"))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU["/help"])
