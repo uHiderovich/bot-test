02631420320fb9544d374ee067f783f541df8113
Гапеевцев Артём <gapeevtsev.a@promail366.com>
2025-05-31 17:33:18 +0800

    end study

--- keyboards/set_menu.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Bot
-
-
-async def set_main_menu(bot: Bot):
-    pass
--- main.py
+++ main.py
@@ -5,14 +5,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
-
-# Импортируем роутеры
-# ...
-# Импортируем миддлвари
-# ...
-# Импортируем вспомогательные функции для создания нужных объектов
-# ...
-from keyboards.set_menu import set_main_menu
+from handlers import other_handlers, user_handlers
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
@@ -33,32 +26,16 @@ async def main():
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
-    # Инициализируем объект хранилища
-    storage = None
-
     # Инициализируем бот и диспетчер
     bot = Bot(
         token=config.tg_bot.token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
-    dp = Dispatcher(storage=storage)
+    dp = Dispatcher()
 
-    # Инициализируем другие объекты (пул соединений с БД, кеш и т.п.)
-    # ...
-
-    # Помещаем нужные объекты в workflow_data диспетчера
-    # dp.workflow_data.update(...)
-
-    # Настраиваем главное меню бота
-    await set_main_menu(bot)
-
-    # Регистриуем роутеры
-    logger.info("Подключаем роутеры")
-    # ...
-
-    # Регистрируем миддлвари
-    logger.info("Подключаем миддлвари")
-    # ...
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
