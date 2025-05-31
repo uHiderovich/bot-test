1f0fedd053925bf6a6e273f895d4929d204a6c69
Гапеевцев Артём <gapeevtsev.a@promail366.com>
2025-05-31 15:05:42 +0800

    end study

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from config_data.config import Config, load_config
-
-# Импортируем роутеры
-# ...
-# Импортируем миддлвари
-# ...
-# Импортируем вспомогательные функции для создания нужных объектов
-# ...
-from keyboards.set_menu import set_main_menu
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    KeyboardButtonPollType,
+)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types.web_app_info import WebAppInfo
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
+
+dp = Dispatcher()
 
 
 # Функция конфигурирования и запуска бота
@@ -33,36 +35,113 @@ async def main():
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
-    # Инициализируем объект хранилища
-    storage = None
-
     # Инициализируем бот и диспетчер
-    bot = Bot(
-        token=config.tg_bot.token,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
-    )
-    dp = Dispatcher(storage=storage)
+    bot = Bot(token=config.tg_bot.token)
 
-    # Инициализируем другие объекты (пул соединений с БД, кеш и т.п.)
-    # ...
+    # Создаем объекты кнопок
+    # kb_builder = ReplyKeyboardBuilder()
+    # button_1 = KeyboardButton(text="Собак 🦮")
+    # button_2 = KeyboardButton(text="Огурцов 🥒")
 
-    # Помещаем нужные объекты в workflow_data диспетчера
-    # dp.workflow_data.update(...)
+    # buttons: list[KeyboardButton] = [
+    #     KeyboardButton(text=f"Кнопка {i + 1}") for i in range(10)
+    # ]
 
-    # Настраиваем главное меню бота
-    await set_main_menu(bot)
+    # kb_builder.row(*buttons, width=4)
+    # kb_builder.add(*buttons)
 
-    # Регистриуем роутеры
-    logger.info("Подключаем роутеры")
-    # ...
+    # kb_builder.row(*buttons)
 
-    # Регистрируем миддлвари
-    logger.info("Подключаем миддлвари")
-    # ...
+    # В первом ряду 1 кнопка, в последующих 3 кнопки
+    # kb_builder.adjust(1, 3)
 
-    # Пропускаем накопившиеся апдейты и запускаем polling
-    await bot.delete_webhook(drop_pending_updates=True)
+    # Создаем объект клавиатуры, добавляя в него кнопки
+    # keyboard = ReplyKeyboardMarkup(
+    #     keyboard=[[button_1, button_2]], resize_keyboard=True, one_time_keyboard=True
+    # )
+
+    # Инициализируем билдер
+    # kb_builder = ReplyKeyboardBuilder()
+
+    # # Создаем кнопки
+    # contact_btn = KeyboardButton(text="Отправить телефон", request_contact=True)
+    # geo_btn = KeyboardButton(text="Отправить геолокацию", request_location=True)
+    # poll_btn = KeyboardButton(
+    #     text="Создать опрос/викторину", request_poll=KeyboardButtonPollType()
+    # )
+
+    # # Добавляем кнопки в билдер
+    # kb_builder.row(contact_btn, geo_btn, poll_btn, width=1)
+
+    # # Создаем объект клавиатуры
+    # keyboard: ReplyKeyboardMarkup = kb_builder.as_markup(
+    #     resize_keyboard=True, one_time_keyboard=True
+    # )
+
+    # Создаем список списков с кнопками
+    keyboard: list[list[KeyboardButton]] = [
+        [KeyboardButton(text=str(i)) for i in range(1, 4)],
+        [KeyboardButton(text=str(i)) for i in range(4, 7)],
+        [KeyboardButton(text=str(i)) for i in range(7, 9)],
+    ]
+
+    # Создаем объект клавиатуры, добавляя в него кнопки
+    my_keyboard = ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+
+    # Этот хэндлер будет срабатывать на команду "/start"
+    @dp.message(CommandStart())
+    async def process_start_command(message: Message):
+        await message.answer(
+            text="Экспериментируем со специальными кнопками", reply_markup=my_keyboard
+        )
+
+    # Создаем кнопку
+    # web_app_btn = KeyboardButton(
+    #     text="Start Web App", web_app=WebAppInfo(url="https://stepik.org/")
+    # )
+    # # Создаем объект клавиатуры
+    # web_app_keyboard = ReplyKeyboardMarkup(
+    #     keyboard=[[web_app_btn]], resize_keyboard=True
+    # )
+
+    # # Этот хэндлер будет срабатывать на команду "/web_app"
+    # @dp.message(Command(commands="web_app"))
+    # async def process_web_app_command(message: Message):
+    #     await message.answer(
+    #         text="Экспериментируем со специальными кнопками",
+    #         reply_markup=web_app_keyboard,
+    #     )
+
+    # Этот хэндлер будет срабатывать на команду "/start"
+    # и отправлять в чат клавиатуру
+    # @dp.message(CommandStart())
+    # async def process_start_command(message: Message):
+    #     # await message.answer(text="Чего кошки боятся больше?", reply_markup=keyboard)
+
+    #     await message.answer(
+    #         text="Вот такая получается клавиатура",
+    #         reply_markup=kb_builder.as_markup(resize_keyboard=True),
+    #     )
+
+    # # Этот хэндлер будет срабатывать на ответ "Собак 🦮" и удалять клавиатуру
+    # @dp.message(F.text == "Собак 🦮")
+    # async def process_dog_answer(message: Message):
+    #     await message.answer(
+    #         text="Да, несомненно, кошки боятся собак. "
+    #         "Но вы видели как они боятся огурцов?",
+    #         reply_markup=ReplyKeyboardRemove(),
+    #     )
+
+    # # Этот хэндлер будет срабатывать на ответ "Огурцов 🥒" и удалять клавиатуру
+    # @dp.message(F.text == "Огурцов 🥒")
+    # async def process_cucumber_answer(message: Message):
+    #     await message.answer(
+    #         text="Да, иногда кажется, что огурцов " "кошки боятся больше",
+    #         reply_markup=ReplyKeyboardRemove(),
+    #     )
+
     await dp.start_polling(bot)
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
