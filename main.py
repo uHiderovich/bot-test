d97766dbc8704161aa3f6744c6e7b0977f62f16b
Гапеевцев Артём <gapeevtsev.a@promail366.com>
2025-06-08 19:12:22 +0800

    end study

--- main.py
+++ main.py
@@ -4,18 +4,31 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+
+from aiogram.types import Message
 from config_data.config import Config, load_config
 
-# Импортируем роутеры
-# ...
-# Импортируем миддлвари
-# ...
-# Импортируем вспомогательные функции для создания нужных объектов
-# ...
-from keyboards.set_menu import set_main_menu
+from keyboards.create_inline_kb import create_inline_kb
+
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
+
+
+BUTTONS: dict[str, str] = {
+    "btn_1": "1",
+    "btn_2": "2",
+    "btn_3": "3",
+    "btn_4": "4",
+    "btn_5": "5",
+    "btn_6": "6",
+    "btn_7": "7",
+    "btn_8": "8",
+    "btn_9": "9",
+    "btn_10": "10",
+    "btn_11": "11",
+}
 
 
 # Функция конфигурирования и запуска бота
@@ -33,24 +46,12 @@ async def main():
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
-    # Инициализируем объект хранилища
-    storage = None
-
-    # Инициализируем бот и диспетчер
+    # Создаем объекты бота и диспетчера
     bot = Bot(
         token=config.tg_bot.token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
-    dp = Dispatcher(storage=storage)
-
-    # Инициализируем другие объекты (пул соединений с БД, кеш и т.п.)
-    # ...
-
-    # Помещаем нужные объекты в workflow_data диспетчера
-    # dp.workflow_data.update(...)
-
-    # Настраиваем главное меню бота
-    await set_main_menu(bot)
+    dp = Dispatcher()
 
     # Регистриуем роутеры
     logger.info("Подключаем роутеры")
@@ -58,10 +59,54 @@ async def main():
 
     # Регистрируем миддлвари
     logger.info("Подключаем миддлвари")
-    # ...
+
+    # Этот хэндлер будет срабатывать на команду "/start"
+    # и отправлять в чат клавиатуру
+    @dp.message(CommandStart())
+    async def process_start_command(message: Message):
+        # keyboard = create_inline_kb(2, "but_1", "but_3", "but_7")
+
+        # keyboard = create_inline_kb(
+        #     2,
+        #     btn_tel="Телефон",
+        #     btn_email="email",
+        #     btn_website="Web-сайт",
+        #     btn_vk="VK",
+        #     btn_tgbot="Наш телеграм-бот",
+        # )
+
+        # keyboard = create_inline_kb(4, last_btn="asdasa", **BUTTONS)
+
+        # keyboard = create_inline_kb(
+        #     3, "but_1", "but_2", "but_3", "but_4", "but_5", last_btn="Последняя кнопка"
+        # )
+
+        # keyboard = create_inline_kb(
+        #     2, last_btn="Последняя кнопка", b_1="1", b_2="2", b_3="3", b_4="4", b_5="5"
+        # )
+
+        # keyboard = create_inline_kb(2, last_btn="Последняя кнопка", *BUTTONS)
+
+        keyboard = create_inline_kb(
+            2,
+            last_btn=None,
+            b_1="1",
+            b_2="2",
+            b_3="3",
+            b_4="4",
+            b_5="5",
+            b_6="Последняя кнопка",
+        )
+
+        await message.answer(
+            text="Это инлайн-клавиатура, сформированная функцией "
+            "<code>create_inline_kb</code>",
+            reply_markup=keyboard,
+        )
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
+
     await dp.start_polling(bot)
 
 
